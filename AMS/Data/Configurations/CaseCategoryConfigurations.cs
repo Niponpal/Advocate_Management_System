273ea27c4a40b54f799284fa2d5a20eddf55a65b
Niponpal <niponpal52@gmail.com>
2026-05-22 16:10:46 +0600

    Fix Configuration 🚀🚀🚑

---
+++ AMS/Data/Configurations/CaseCategoryConfigurations.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AMS.Data.Configurations;
+
+public class CaseCategoryConfigurations: IEntityTypeConfiguration<CaseCategory>
+{
+    public void Configure(EntityTypeBuilder<CaseCategory> builder)
+    {
+            builder.ToTable(nameof(CaseCategory));
+        builder.HasKey(cc => cc.Id);
+        builder.Property(cc => cc.CategoryName)
+            .IsRequired()
+            .HasMaxLength(100);
+        builder.Property(cc => cc.IsActive)
+            .IsRequired();
+    }
+}
