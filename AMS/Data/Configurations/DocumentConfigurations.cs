0444358481c9b8f5188c6f7202debecc366069c1
Niponpal <niponpal52@gmail.com>
2026-05-23 00:11:40 +0600

    Fix Configurations

---
+++ AMS/Data/Configurations/DocumentConfigurations.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AMS.Data.Configurations;
+
+public class DocumentConfigurations: IEntityTypeConfiguration<Document>
+{
+    public void Configure(EntityTypeBuilder<Document> builder)
+    {
+        builder.ToTable(nameof(Document));
+        builder.HasKey(d => d.Id);
+        builder.Property(d => d.DocumentTitle)
+            .IsRequired()
+            .HasMaxLength(200);
+        builder.Property(d => d.FilePath)
+            .IsRequired()
+            .HasMaxLength(500);
+        builder.Property(d => d.DocumentType)
+            .HasMaxLength(100);
+        builder.Property(d => d.UploadDate)
+            .IsRequired();
+        // Relationships
+        builder.HasOne(d => d.Case)
+            .WithMany(c => c.Documents)
+            .HasForeignKey(d => d.CaseId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
