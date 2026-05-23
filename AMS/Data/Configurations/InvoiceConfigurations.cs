3ddda5c101d069c30e3bdbdc87365f3f5ced19e7
Niponpal <niponpal52@gmail.com>
2026-05-23 11:12:14 +0600

    Fix Configurations 🚀🚀🚑

---
+++ AMS/Data/Configurations/InvoiceConfigurations.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AMS.Data.Configurations;
+
+public class InvoiceConfigurations: IEntityTypeConfiguration<Invoice>
+{
+    public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Invoice> builder)
+    {
+        builder.ToTable(nameof(Invoice));
+        builder.HasKey(i => i.Id);
+        builder.Property(i => i.InvoiceNumber)
+            .IsRequired()
+            .HasMaxLength(100);
+        builder.Property(i => i.TotalAmount)
+            .IsRequired()
+            .HasColumnType("decimal(18,2)");
+        builder.Property(i => i.InvoiceDate)
+            .IsRequired();
+        // Relationships
+        builder.HasOne(i => i.Client)
+            .WithMany(c => c.Invoices)
+            .HasForeignKey(i => i.ClientId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
