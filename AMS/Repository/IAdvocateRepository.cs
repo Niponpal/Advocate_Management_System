7c7a512add56b0dfbb21a9b7687d80fbc59dae7f
Niponpal <niponpal52@gmail.com>
2026-05-25 16:50:23 +0600

    Fix IAdvocateRepository  🚀🚀🚑

---
+++ AMS/Repository/IAdvocateRepository.cs
@@ -0,0 +1,42 @@
+﻿namespace AMS.Repository;
+
+public interface IAdvocateRepository
+{
+    // CRUD operations for Application entity
+    Task<IEnumerable<Advocate>> GetAllApplicationsAsync(CancellationToken cancellationToken);
+    Task<Advocate?> GetAdvocateByIdAsync(long id, CancellationToken cancellationToken);
+    Task<Advocate> AddAdvocateAsync(Advocate advocate, CancellationToken cancellationToken);
+    Task<Advocate?> UpdateAdvocateAsync(Advocate advocate, CancellationToken cancellationToken);
+    Task<Advocate> DeleteAdvocateAsync(long id, CancellationToken cancellationToken);
+
+    //Task<bool> IsAlreadyAppliedAsync(long jobId, long userId, CancellationToken cancellationToken);
+    //Task<List<Job>> GetAppliedJobsByUserAsync(long userId, CancellationToken cancellationToken);
+}
+
+public class AdvocateRepository : IAdvocateRepository
+{
+    public Task<Advocate> AddAdvocateAsync(Advocate advocate, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Advocate> DeleteAdvocateAsync(long id, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Advocate?> GetAdvocateByIdAsync(long id, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Advocate>> GetAllApplicationsAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Advocate?> UpdateAdvocateAsync(Advocate advocate, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
