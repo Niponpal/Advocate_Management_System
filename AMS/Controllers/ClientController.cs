0ccf70fff1a0597903bd04a0bfc3ea31827420f8
Niponpal <niponpal52@gmail.com>
2026-05-27 13:16:04 +0600

    Fix ClientRepository 🚀🚀🚑

---
+++ AMS/Controllers/ClientController.cs
@@ -0,0 +1,67 @@
+﻿using AMS.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AMS.Controllers;
+
+public class ClientController : Controller
+{
+    private readonly IClientRepository _clientRepository;
+    public ClientController(IClientRepository clientRepository)
+    {
+        _clientRepository = clientRepository;
+    }
+    public async Task<IActionResult> Index(CancellationToken cancellationToken)
+    {
+        var data = await _clientRepository.GetAllApplicationsAsync(cancellationToken);
+        if (data != null)
+        {
+            return View(data);
+        }
+        return NotFound();
+    }
+    [HttpGet]
+    public async Task<IActionResult>CreateOrEdit(long id, CancellationToken cancellationToken)
+    {
+        if (id == null)
+        {
+            return View(new Client());
+        }
+        var data = await _clientRepository.GetClientByIdAsync(id, cancellationToken);
+        if (data != null)
+        {
+            return View(data);
+        }
+        return NotFound();
+    }
+    [HttpPost]
+    public async Task<IActionResult> CreateOrEdit(Client model, CancellationToken cancellationToken)
+    {
+            if (model.Id == 0)
+            {
+                await _clientRepository.AddClientAsync(model, cancellationToken);
+            }
+            else
+            {
+                await _clientRepository.UpdateClientAsync(model, cancellationToken);
+            }
+            return RedirectToAction(nameof(Index));
+        
+    }
+    [HttpGet]
+    public async Task<IActionResult> Details(long id, CancellationToken cancellationToken)
+    {
+        var data = await _clientRepository.GetClientByIdAsync(id, cancellationToken);
+        if (data != null)
+        {
+            return View(data);
+        }
+        return NotFound();
+    }
+    [HttpPost]
+    public async Task<IActionResult> Delete(long id, CancellationToken cancellationToken)
+    {
+        await _clientRepository.DeleteClientAsync(id, cancellationToken);
+        return RedirectToAction(nameof(Index));
+    }
+ }
---
+++ AMS/Repository/IClientRepository.cs
@@ -0,0 +1,81 @@
+﻿using AMS.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AMS.Repository;
+
+public interface IClientRepository
+{ // CRUD operations for Application entity
+    Task<IEnumerable<Client>> GetAllApplicationsAsync(CancellationToken cancellationToken);
+    Task<Client?> GetClientByIdAsync(long id, CancellationToken cancellationToken);
+    Task<Client> AddClientAsync(Client client, CancellationToken cancellationToken);
+    Task<Client?> UpdateClientAsync(Client client, CancellationToken cancellationToken);
+    Task<Client> DeleteClientAsync(long id, CancellationToken cancellationToken);
+}
+
+public class ClientRepository : IClientRepository
+{
+    private readonly ApplicationDbContext _context;
+    public ClientRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<Client?> UpdateClientAsync(Client client, CancellationToken cancellationToken)
+    {
+        var data = await _context.clients.FindAsync(client.Id, cancellationToken);
+        if (data != null)
+        {
+            data.ClientName = client.ClientName;
+            data.Email = client.Email;
+            data.Phone = client.Phone;
+            data.Address = client.Address;
+            data.NIDNumber = client.NIDNumber;
+            data.Cases = client.Cases;
+            data.Payments = client.Payments;
+            data.Appointments = client.Appointments;
+            data.Invoices = client.Invoices;
+            data.LegalNotices = client.LegalNotices;
+            await _context.SaveChangesAsync(cancellationToken);
+            return data;
+        }
+        else
+        {
+            throw new Exception("Client not found");
+        }
+    }
+    async Task<Client> IClientRepository.AddClientAsync(Client client, CancellationToken cancellationToken)
+    {
+      var data = await _context.clients.AddAsync(client, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+        return client;
+    }
+    async Task<Client> IClientRepository.DeleteClientAsync(long id, CancellationToken cancellationToken)
+    {
+        var data = await _context.clients.FindAsync(id, cancellationToken);
+        if (data != null)
+        {
+            _context.clients.Remove(data);
+            await _context.SaveChangesAsync(cancellationToken);
+            return data;
+        }
+        else
+        {
+            throw new Exception("Client not found");
+        }
+    }
+    public async Task<Client?> GetClientByIdAsync(long id, CancellationToken cancellationToken)
+    {
+        var data = await _context.clients.FindAsync(id, cancellationToken);
+        if (data != null)
+        {
+            return data;
+        }
+        else
+        {
+            throw new Exception("Client not found");
+        }
+    }
+    public async Task<IEnumerable<Client>> GetAllApplicationsAsync(CancellationToken cancellationToken)
+    {
+       return await _context.clients.ToListAsync(cancellationToken);
+    }
+}
