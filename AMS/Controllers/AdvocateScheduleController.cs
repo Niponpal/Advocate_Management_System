353db660f46899da877ba43da9d8bd7edd960703
Niponpal <niponpal52@gmail.com>
2026-05-27 12:57:24 +0600

    Fix All 🚀🚀🚑

--- AMS/Controllers/AdvocateScheduleController.cs
+++ AMS/Controllers/AdvocateScheduleController.cs
@@ -21,4 +21,56 @@ public class AdvocateScheduleController : Controller
         }
         return NotFound();
     }
+      [HttpGet]
+        public async Task<IActionResult> CreateOrEdit(long id, CancellationToken cancellationToken)
+        {
+            if (id == 0)
+            {
+                return View(new AdvocateSchedule());
+            }
+            else
+            {
+                var data = await _advocateScheduleRepository.GetAdvocateByIdAsync(id, cancellationToken);
+                if (data != null)
+                {
+                    return View(data);
+                }
+                return NotFound();
+            }
+    }
+    [HttpPost]
+    public async Task<IActionResult> CreateOrEdit(AdvocateSchedule advocateSchedule, CancellationToken cancellationToken)
+    {
+        if (ModelState.IsValid)
+        {
+            if (advocateSchedule.Id == 0)
+            {
+                await _advocateScheduleRepository.AddAdvocateAsync(advocateSchedule, cancellationToken);
+            }
+            else
+            {
+                await _advocateScheduleRepository.UpdateAdvocateAsync(advocateSchedule, cancellationToken);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(advocateSchedule);
+    }
+    [HttpPost]
+    public async Task<IActionResult> Delete(long id, CancellationToken cancellationToken)
+    {
+        await _advocateScheduleRepository.DeleteAdvocateAsync(id, cancellationToken);
+        return RedirectToAction(nameof(Index));
+    }
+    [HttpGet]
+
+    public async Task<IActionResult> Details(long id, CancellationToken cancellationToken)
+    {
+        var data = await _advocateScheduleRepository.GetAdvocateByIdAsync(id, cancellationToken);
+        if (data != null)
+        {
+            return View(data);
+        }
+        return NotFound();
+    }
+
 }
---
+++ AMS/Controllers/CaseCategoryController.cs
@@ -0,0 +1,72 @@
+﻿using AMS.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AMS.Controllers;
+
+public class CaseCategoryController : Controller
+{
+    private readonly ICaseCategoryRepository _categoryRepository;
+
+    public CaseCategoryController(ICaseCategoryRepository categoryRepository)
+    {
+        _categoryRepository = categoryRepository;
+    }
+
+    public async Task<IActionResult> Index(CancellationToken cancellationToken)
+    {
+        var data = await _categoryRepository.GetAllCaseCategoryAsync(cancellationToken);
+        if (data != null)
+        {
+            return View(data);
+        }
+        return NotFound();
+    }
+    [HttpGet]
+    public async Task<IActionResult> CreateOrEdit(long id, CancellationToken cancellationToken)
+    {
+        if (id == 0)
+        {
+            return View();
+        }
+        var data = await _categoryRepository.GetCaseCategoryByIdAsync(id, cancellationToken);
+        if (data != null)
+        {
+            return View(data);
+        }
+        return NotFound();
+    }   
+    [HttpPost]
+    public async Task<IActionResult> CreateOrEdit(CaseCategory category, CancellationToken cancellationToken)
+    {
+        if (ModelState.IsValid)
+        {
+            if (category.Id == 0)
+            {
+                await _categoryRepository.AddCaseCategoryAsync(category, cancellationToken);
+            }
+            else
+            {
+                await _categoryRepository.UpdateCaseCategoryAsync(category, cancellationToken);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(category);
+    }
+    [HttpPost]
+    public async Task<IActionResult> Delete(long id, CancellationToken cancellationToken)
+    {
+        await _categoryRepository.DeleteCaseCategoryAsync(id, cancellationToken);
+        return RedirectToAction(nameof(Index));
+    }
+    [HttpGet]
+    public async Task<IActionResult> Details(long id, CancellationToken cancellationToken)
+    {
+        var data = await _categoryRepository.GetCaseCategoryByIdAsync(id, cancellationToken);
+        if (data != null)
+        {
+            return View(data);
+        }
+        return NotFound();
+    }
+}
