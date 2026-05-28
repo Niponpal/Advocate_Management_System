63fc26991bb8cfe1c0b619913b5ad73d9c63235f
Niponpal <niponpal52@gmail.com>
2026-05-28 17:57:17 +0600

    Fix ITaskManagementRepository 🚀🚀🚑

---
+++ AMS/Repository/ITaskManagementRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace AMS.Repository;
+
+public interface ITaskManagementRepository
+{
+    // CRUD operations for Task Management entity
+    Task<IEnumerable<TaskManagement>> GetAllTasksAsync(CancellationToken cancellationToken);
+    Task<TaskManagement> GetTaskByIdAsync(long id, CancellationToken cancellationToken);
+    Task<TaskManagement> AddTaskAsync(TaskManagement taskManagement, CancellationToken cancellationToken);
+    Task<TaskManagement> UpdateTaskAsync(TaskManagement taskManagement, CancellationToken cancellationToken);
+    Task<TaskManagement> DeleteTaskAsync(long id, CancellationToken cancellationToken);
+}
