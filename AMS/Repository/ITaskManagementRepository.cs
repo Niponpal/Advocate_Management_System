212e5b6f1b4b6105331f281df28f129d8815f87c
Niponpal <niponpal52@gmail.com>
2026-05-28 18:08:26 +0600

    Fix TaskManagementRepository 🚀🚀🚑

--- AMS/Repository/ITaskManagementRepository.cs
+++ AMS/Repository/ITaskManagementRepository.cs
@@ -1,4 +1,7 @@
-﻿namespace AMS.Repository;
+﻿using AMS.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AMS.Repository;
 
 public interface ITaskManagementRepository
 {
@@ -8,4 +11,62 @@ public interface ITaskManagementRepository
     Task<TaskManagement> AddTaskAsync(TaskManagement taskManagement, CancellationToken cancellationToken);
     Task<TaskManagement> UpdateTaskAsync(TaskManagement taskManagement, CancellationToken cancellationToken);
     Task<TaskManagement> DeleteTaskAsync(long id, CancellationToken cancellationToken);
+}
+
+public class TaskManagementRepository : ITaskManagementRepository
+{
+    private readonly ApplicationDbContext _context;
+    public TaskManagementRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<TaskManagement> AddTaskAsync(TaskManagement taskManagement, CancellationToken cancellationToken)
+    {
+        var data = await _context.taskManagements.AddAsync(taskManagement, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+        return taskManagement;
+    }
+    public async Task<TaskManagement> DeleteTaskAsync(long id, CancellationToken cancellationToken)
+    {
+        var data = await _context.taskManagements.FindAsync(id, cancellationToken);
+        if (data != null)
+        {
+            _context.taskManagements.Remove(data);
+            await _context.SaveChangesAsync(cancellationToken);
+            return data;
+        }
+        else
+        {
+            throw new Exception("Task not found");
+        }
+    }
+    public async Task<IEnumerable<TaskManagement>> GetAllTasksAsync(CancellationToken cancellationToken)
+    {
+       var data = await _context.taskManagements.ToListAsync(cancellationToken);
+        return data;
+    }
+    public async Task<TaskManagement> GetTaskByIdAsync(long id, CancellationToken cancellationToken)
+    {
+       var data = await _context.taskManagements.FindAsync(id, cancellationToken);
+        return data;
+    }
+    public async Task<TaskManagement> UpdateTaskAsync(TaskManagement taskManagement, CancellationToken cancellationToken)
+    {
+        var data = await _context.taskManagements.FindAsync(taskManagement.Id, cancellationToken);
+        if (data != null)
+        {
+            data.TaskTitle = taskManagement.TaskTitle;
+            data.Description = taskManagement.Description;
+            data.DueDate = taskManagement.DueDate;
+            data.Status = taskManagement.Status;
+            data.AdvocateId = taskManagement.AdvocateId;
+            data.Advocate = taskManagement.Advocate;
+            await _context.SaveChangesAsync(cancellationToken);
+            return data;
+        }
+        else
+        {
+            throw new Exception("Task not found");
+        }
+    }
 }
