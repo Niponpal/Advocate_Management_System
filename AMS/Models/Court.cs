7da97c60fd780d756a41fdafa8bf37fe2749867f
Niponpal <niponpal52@gmail.com>
2026-06-10 12:43:32 +0600

    Fix Court Enitty 🚀🚀🚑

--- AMS/Models/Court.cs
+++ AMS/Models/Court.cs
@@ -1,12 +1,13 @@
 ﻿using AMS.BaseEntities;
-using AMS.Models;
 
-public class Court:BaseEntity
+namespace AMS.Models;
+
+public class Court: BaseEntity
 {
+    public string CourtName { get; set; }
+    public string CourtType { get; set; }
+    public string Location { get; set; }
 
-public string CourtName { get; set; }
-public string CourtType { get; set; }
-public string Location { get; set; }
+    public ICollection<Hearing> Hearings { get; set; }
 
-public  ICollection<Hearing> Hearings { get; set; }
 }
