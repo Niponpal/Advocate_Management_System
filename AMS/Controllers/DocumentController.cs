5f036f635a20a36e059c8328ab08bbfa67debe17
Niponpal <niponpal52@gmail.com>
2026-06-13 23:35:16 +0600

    Fix CreateOrEdit 🚀🚀🚑

--- AMS/Controllers/DocumentController.cs
+++ AMS/Controllers/DocumentController.cs
@@ -22,19 +22,23 @@ public class DocumentController : Controller
         }
         return NotFound();
     }
+ 
     [HttpGet]
     public async Task<IActionResult> CreateOrEdit(long id, CancellationToken cancellationToken)
     {
-        if (id == null)
+        if (id == 0)
         {
             return View(new Document());
         }
+
         var data = await documentRepository.GetDocumentByIdAsync(id, cancellationToken);
-        if (data != null)
+
+        if (data == null)
         {
-            return View(data);
+            return NotFound();
         }
-        return NotFound();
+
+        return View(data);
     }
     [HttpPost]
     public async Task<IActionResult> CreateOrEdit(Document document, CancellationToken cancellationToken)
