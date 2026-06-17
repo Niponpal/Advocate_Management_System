90fd3aa35990af981e2590e4bcb2852ff9f2dcd4
Niponpal <niponpal52@gmail.com>
2026-06-18 01:05:37 +0600

    Fix LegalNoticeController 🚀🚀🚑

---
+++ AMS/Controllers/LegalNoticeController.cs
@@ -0,0 +1,20 @@
+﻿using AMS.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AMS.Controllers;
+
+public class LegalNoticeController : Controller
+{
+    private readonly ILegalNoticeRepository _legalNoticeRepository;
+
+    public LegalNoticeController(ILegalNoticeRepository legalNoticeRepository)
+    {
+        _legalNoticeRepository = legalNoticeRepository;
+    }
+    public async Task<IActionResult> Index(CancellationToken cancellationToken)
+    {
+        var data = await _legalNoticeRepository.GetAllLegalNoticeAsync(cancellationToken);
+        return View();
+    }
+}
