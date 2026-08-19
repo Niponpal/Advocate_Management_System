6c97b941adfe8c8778303f370a451c003122c340
Niponpal <niponpal52@gmail.com>
2026-08-20 00:14:42 +0600

    Fix All 🚀🚀🚑

--- AMS/Models/TaskManagement.cs
+++ AMS/Models/TaskManagement.cs
@@ -8,8 +8,6 @@ public class TaskManagement: BaseEntity
     public string Description { get; set; }
     public DateTime DueDate { get; set; }
     public string Status { get; set; }
-
     public long AdvocateId { get; set; }
-
     public Advocate Advocate { get; set; }
 }
